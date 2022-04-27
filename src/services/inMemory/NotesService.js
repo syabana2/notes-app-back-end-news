9b251f36ace0a9fc21968365e05ded605a7d7efc
syaban <rizkisyaban2@gmail.com>
2022-04-27 16:23:20 +0700

    data validation implemeted

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,4 +1,6 @@
 const {nanoid} = require('nanoid');
+const InvariantError = require('../../exceptions/InvatiantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 
 class NotesService {
@@ -20,7 +22,7 @@ class NotesService {
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
     if (!isSuccess) {
-      throw new Error('Catatan gagal ditambahkan');
+      throw new InvariantError('Catatan gagal ditambahkan');
     }
 
     return id;
@@ -33,7 +35,7 @@ class NotesService {
   getNoteById(id) {
     const note = this._notes.filter((n) => n.id === id)[0];
     if (!note) {
-      throw new Error('Catatan tidak ditemukan');
+      throw new NotFoundError('Catatan tidak ditemukan');
     }
     return note;
   }
@@ -42,7 +44,7 @@ class NotesService {
     const index = this._notes.findIndex((note) => note.id === id);
 
     if (index === -1) {
-      throw new Error('Gagal memperbaharui catatan. Id tidak ditemukan');
+      throw new NotFoundError('Gagal memperbaharui catatan. Id tidak ditemukan');
     }
 
     const updatedAt = new Date().toISOString();
@@ -60,7 +62,7 @@ class NotesService {
     const index = this._notes.findIndex((note) => note.id === id);
 
     if (index === -1) {
-      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
     }
 
     this._notes.splice(index, 1);
